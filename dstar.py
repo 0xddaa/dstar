7146dc383c65c446280c85245414a82fa4360cb4
chengtc <chengtc@cs.nctu.edu.tw>
2015-03-07 19:11:37 +0800

    dstar.py: sperate read file and get all bbls

--- dstar.py
+++ dstar.py
@@ -6,33 +6,35 @@ import sys
 
 LOGPATH = "/home/dada/pin/CodeCoverage/log/"
 BINARY = "bof1"
+COVs = []
 BBLs = []
-
-def parse_report(report):
-    global BBLs
-    jr = json.loads(report)
-    bbls = jr["basic_blocks_info"]["list"]
-    for bbl in bbls:
-        if bbl["address"] not in BBLs:
-            BBLs.append(str(bbl["address"]))
     
 def get_all_bbls():
-    p = sp.Popen("ls %s" % (LOGPATH + BINARY + "*"), shell=True, stdout=sp.PIPE)
+    global BBLs
+
+    for cov in COVs:
+        bbls = cov["basic_blocks_info"]["list"]
+        for bbl in bbls:
+            if bbl["address"] not in BBLs:
+                BBLs.append(str(bbl["address"]))
+
+def get_covs():
+    p = sp.Popen("ls %s" % (LOGPATH + BINARY + "*"), shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+    assert p.stderr.read().strip() == "", "benchmark %s not found" % (BINARY)
     logs = p.stdout.read().strip().split("\n")
 
     for log in logs:
         with open(log, "r") as f:
-            report = f.read()
-            parse_report(report)
-
-    print BBLs
+            COVs.append(json.loads(f.read()))
 
 def main(argc, argv):
     global BINARY
     if argc == 2:
         BINARY = argv[1]
-        
+
+    get_covs()
     get_all_bbls()
+    print BBLs
 
 
 main(len(sys.argv), sys.argv)    
